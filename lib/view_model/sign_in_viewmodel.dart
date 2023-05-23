ca36e698bc2150a93beabbe8c5ec17690fb1806c
Alshimaa Refaat <Alshimaa.Refaat-External@alfuttaim.com>
2023-05-23 16:23:06 +0300

    committing missed file

---
+++ lib/view_model/sign_in_viewmodel.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_provider/models/enum.dart';
+import 'package:flutter_provider/models/sign_in_model.dart';
+
+import 'package:flutter_provider/presnetations/api_call.dart';
+
+import 'package:flutter_provider/utils/common_utils.dart';
+
+import '../models/sign_in_response.dart';
+
+
+
+class SignInViewModel extends ChangeNotifier {
+  Status _status = Status.none;
+
+  Status get status => _status;
+
+  late SignInResponse _signInResponse;
+
+  SignInResponse get signInResponse => _signInResponse;
+
+
+  Future<void> signIn(SignInBody body) async {
+    CommonUtil().checkInternetConnection().then((value) {
+      if (value) {
+        _status = Status.loading;
+        notifyListeners();
+
+        APICall().signIn(body).then((response) {
+          _signInResponse =
+              SignInResponse.fromJson(json.decode(response.body));
+          if (_signInResponse.status == true) {
+            _status = Status.success;
+            notifyListeners();
+          } else {
+            _status = Status.failed;
+            notifyListeners();
+          }
+        });
+      } else {
+        _status = Status.noInternet;
+      }
+    });
+  }
+}
